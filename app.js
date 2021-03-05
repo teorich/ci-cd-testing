7c4cf36b24263d203f61f2903bb875ef5c3e8e6d
Theo <theo.foncham@gmail.com>
2021-03-05 19:12:28 +0100

    new

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('hello world from YDE');
+const express = require('express');
+const app = express();
+
+app.get('/users/test', (req, res) => {
+  res.send({ msg: 'Testing' });
+});
+
+app.listen(5000, () => console.log('Runing on port 5000'));
